0adb66c51d06b2c7ca20a2cf7365a1b99155b67f
Katie McLaughlin <katie@glasnt.com>
2020-06-29 15:17:09 +1000

    ⬇️🦄

--- users/migrations/0002_create_superuser.py
+++ users/migrations/0002_create_superuser.py
@@ -5,22 +5,16 @@ from django.db import migrations
 """
 import google.auth
 from google.cloud import secretmanager_v1beta1 as sm
-
-
 def access_secrets(secret_keys):
     secrets = {}
     _, project = google.auth.default()
-
     if project:
         client = sm.SecretManagerServiceClient()
-
         for s in secret_keys:
             path = client.secret_version_path(project, s, "latest")
             payload = client.access_secret_version(path).payload.data.decode("UTF-8")
             secrets[s] = payload
-
     return secrets
-
 """
 def createsuperuser(apps, schema_editor):
     settings = ["ADMINEMAIL", "ADMINPASS"]
