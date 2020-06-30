918383d2fa761a7316f655055a4773cc10cdf31a
Katie McLaughlin <katie@glasnt.com>
2020-06-30 15:19:40 +1000

    Give datastore more time, show if it's all ready

--- .gcloud/postcreate.sh
+++ .gcloud/postcreate.sh
@@ -12,10 +12,22 @@ echo ""
 echo ""
 echo "Mewgram is now deployed to ${SERVICE_URL}"
 echo ""
-echo "To login into the Django admin: "
+
+# Helpful added message if datastore indexes are still processing
+indexes_creating=$(gcloud datastore indexes list --format "value(kind)" --filter "state=CREATING" | wc -l)
+
+if [ $indexes_creating -ne 0 ]: then 
+    echo "⚠️  Datastore is still processing, so you may need to wait a few minutes for that to finish."
+    echo "   You can check it's ready by checking:"
+    echo "gcloud datastore indexes list --format 'value(state, kind)'"
+    echo ""
+fi
+
+echo "ℹ️  To login into the Django admin: "
 echo "  * go to ${SERVICE_URL}/admin"
 echo "  * login with the username and password that are stored in Secret Manager"
 echo ""
 echo "gcloud secrets versions access latest --secret ADMINEMAIL"
 echo "gcloud secrets versions access latest --secret ADMINPASS"
 echo ""
+
--- .gcloud/precreate.sh
+++ .gcloud/precreate.sh
@@ -3,8 +3,7 @@
 echo "🚀 Running initial Django migration (this will take a few minutes)..."
 echo "   Configurations: service ${K_SERVICE}, region ${GOOGLE_CLOUD_REGION}"
 
-gcloud builds submit
-
 gcloud datastore indexes create index.yaml -q
+gcloud builds submit
 
 echo "Pre-create data migration complete ✨"
