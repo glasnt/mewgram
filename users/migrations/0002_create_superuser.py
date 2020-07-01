c3bc926b59994c19111355d146e25a0203b22843
Katie McLaughlin <katie@glasnt.com>
2020-07-02 09:49:59 +1000

    Use LOCAL_DEVELOPMENT setting

--- users/migrations/0002_create_superuser.py
+++ users/migrations/0002_create_superuser.py
@@ -2,19 +2,24 @@ import os
 from django.contrib.auth import get_user_model
 
 from django.db import migrations
+from django.conf import settings
 
-import google.auth
-from google.cloud import secretmanager_v1beta1 as sm
-_, project = google.auth.default()
+
+
 
 def access_secret(secret_key):
-    if project and not os.path.isfile('.env'):
+    if settings.LOCAL_DEVELOPMENT:
+        return os.environ[secret_key]
+
+    else:
+        import google.auth
+        from google.cloud import secretmanager_v1beta1 as sm
+        _, project = google.auth.default()
+
         client = sm.SecretManagerServiceClient()
         path = client.secret_version_path(project, secret_key, "latest")
         payload = client.access_secret_version(path).payload.data.decode("UTF-8")
         return payload
-    else:
-        return os.environ[secret_key]
 
 def createsuperuser(apps, schema_editor):
     email = access_secret("ADMINEMAIL")
